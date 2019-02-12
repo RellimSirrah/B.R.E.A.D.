c9f80012b34e9d2b35cfad0014e287abb693daec
Harris Miller <harriswmiller@gmail.com>
2019-02-12 16:11:44 -0500

    Added heart indicator and bouncing off enemies

--- Assets/Scripts/PlayerControl1.cs
+++ Assets/Scripts/PlayerControl1.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class PlayerControl1 : MonoBehaviour
 {
-
+    public GameObject heart1, heart2, heart3, heart4, heart5;
     bool grounded;
     bool isJumping;
     float speed;
@@ -19,17 +19,19 @@ public class PlayerControl1 : MonoBehaviour
     public Sprite left_jump;
     public Sprite right_punch;
     public Sprite left_punch;
+    Rigidbody2D rb; 
 
     private SpriteRenderer spriteRenderer; 
 
     public void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         grounded = false;
         isJumping = false;
         speed = 12f;
         speed_crouch = speed/2;
-        health_max = 3f;
-        health = 5f;
+        health_max = 5f;
+        health = 3f;
         spriteRenderer = GetComponent<SpriteRenderer>( ); // we are accessin g  the SpriteRenderer that is attached to the Gameobject
         if (spriteRenderer.sprite == null) // if the sprite on spriteRenderer is null then
             spriteRenderer.sprite = right;
@@ -37,6 +39,56 @@ public class PlayerControl1 : MonoBehaviour
 
 
     public void Update()
+    {
+        switch (health)
+        {
+            case 5:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(true);
+                heart3.gameObject.SetActive(true);
+                heart4.gameObject.SetActive(true);
+                heart5.gameObject.SetActive(true);
+                break;
+            case 4:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(true);
+                heart3.gameObject.SetActive(true);
+                heart4.gameObject.SetActive(true);
+                heart5.gameObject.SetActive(false);
+                break;
+            case 3:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(true);
+                heart3.gameObject.SetActive(true);
+                heart4.gameObject.SetActive(false);
+                heart5.gameObject.SetActive(false);
+                break;
+            case 2:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(true);
+                heart3.gameObject.SetActive(false);
+                heart4.gameObject.SetActive(false);
+                heart5.gameObject.SetActive(false);
+                break;
+            case 1:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(false);
+                heart3.gameObject.SetActive(false);
+                heart4.gameObject.SetActive(false);
+                heart5.gameObject.SetActive(false);
+                break;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (isJumping == false)
+            {
+                rb.AddForce(Vector2.up * 25f, ForceMode2D.Impulse);
+                isJumping = true;
+            }
+        }
+    }
+
+    public void FixedUpdate()
     {
         if(health <= 0f)
         {
@@ -48,9 +100,8 @@ public class PlayerControl1 : MonoBehaviour
             isJumping = false;
             Application.LoadLevel(Application.loadedLevel);
         }
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
 
-        if(Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             if (Input.GetKey(KeyCode.LeftShift))
             {
@@ -64,7 +115,7 @@ public class PlayerControl1 : MonoBehaviour
                 transform.Translate(-Vector2.right * speed * Time.deltaTime);
             }
         }
-        else 
+        else
         if (Input.GetKey(KeyCode.D))
         {
             if (Input.GetKey(KeyCode.LeftShift))
@@ -79,39 +130,30 @@ public class PlayerControl1 : MonoBehaviour
                 transform.Translate(Vector2.right * speed * Time.deltaTime);
             }
         }
-        else 
+        else
         if (Input.GetKey(KeyCode.LeftShift))
         {
             if (spriteRenderer.sprite == left)
             {
                 spriteRenderer.sprite = left_crouch;
             }
-            else 
-            if(spriteRenderer.sprite == right)
+            else
+            if (spriteRenderer.sprite == right)
             {
                 spriteRenderer.sprite = right_crouch;
             }
         }
-        else 
+        else
         if (!Input.GetKey(KeyCode.LeftShift))
         {
             if (spriteRenderer.sprite == left_crouch)
             {
                 spriteRenderer.sprite = left;
             }
-            else 
+            else
             if (spriteRenderer.sprite == right_crouch)
             {
                 spriteRenderer.sprite = right;
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            if (isJumping==false)
-            {
-                rb.AddForce(Vector2.up * 25f, ForceMode2D.Impulse);
-                isJumping = true;
             }
         }
 
@@ -147,7 +189,26 @@ public class PlayerControl1 : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.collider.tag == "ground")
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x-0.25f,transform.position.y-0.9f), -Vector2.up,0.5f);
+        RaycastHit2D hit2 = Physics2D.Raycast(new Vector2(transform.position.x + 0.25f, transform.position.y - 0.9f), -Vector2.up, 0.5f);
+        if (hit.collider != null && hit.transform.tag == "enemy")
+        {
+            rb.AddForce(Vector2.up * 25f, ForceMode2D.Impulse);
+            Destroy(hit.transform.gameObject);
+            Debug.Log("hit Left");
+        }
+        else if (hit2.collider != null && hit2.transform.tag == "enemy")
+        {
+            rb.AddForce(Vector2.up * 25f, ForceMode2D.Impulse);
+            Destroy(hit2.transform.gameObject);
+            Debug.Log("hit Right");
+        }
+        else if(col.collider.tag == "enemy")
+        {
+            health--;
+        }
+
+        if (col.gameObject.layer == 8)
         {
             isJumping = false;
         }
@@ -157,10 +218,6 @@ public class PlayerControl1 : MonoBehaviour
             {
                 health++;
             }
-        }
-        if (col.collider.tag == "enemy")
-        {
-            health--;
         }
         if (col.collider.tag == "proj" && !Input.GetKey(KeyCode.LeftShift))
         {
